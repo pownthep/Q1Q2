19c9df78d538db0eecefba14675b945439f74978
Pownthep Laokhunthot <pownthepofficial@gmail.com>
2021-02-04 05:48:39 -0800

    finished question 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,59 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'section2';
+  public inputValue = 0;
+  modeObjects: mode[];
+  selectedMode: mode | undefined;
+  output: boolean | undefined;
+
+  constructor() {
+    this.modeObjects = [
+      { id: 1, name: 'isPrime' },
+      { id: 2, name: 'isFibonacci' },
+    ];
+  }
+
+  handleInput(e: any) {
+    const input = Math.round(e.target.value);
+    if (input < 0) this.inputValue = 1;
+    else {
+      this.inputValue = input;
+    }
+    if (this.selectedMode) this.updateOutput();
+  }
+
+  handleSelect() {
+    this.updateOutput();
+  }
+  isPrime(num: number) {
+    for (var i = 2; i < num; i++) if (num % i === 0) return false;
+    return num > 1;
+  }
+  isFibonacci(query: number, count = 1, last = 0): boolean {
+    if (count < query) {
+      return this.isFibonacci(query, count + last, count);
+    }
+    if (count === query) {
+      return true;
+    }
+    return false;
+  }
+
+  updateOutput() {
+    if (this.selectedMode?.id === 1)
+      this.output = this.isPrime(this.inputValue);
+    else this.output = this.isFibonacci(this.inputValue);
+  }
+}
+
+interface mode {
+  id: number;
+  name: string;
 }
